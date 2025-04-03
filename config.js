57a5364e35d0bebde71b45327859becd212d5b50
francoxortiz1975 <francoxortiz2005@gmail.com>
2025-04-03 14:45:42 +0200

    autorization keys 2

---
+++ config.js
@@ -0,0 +1,16 @@
+// Añade un console.log para verificar
+console.log('Proceso ENV:', process.env);
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
+
+console.log('Firebase Config:', firebaseConfig);
+
+export default firebaseConfig;
